7f919fe230bbd403f1421e9594d481a825c1b67b
Oleh Senkiv <33096162+senkiv-oleh@users.noreply.github.com>
2025-10-19 01:20:07 +0300

    Split code into reusable components and improve code style (#7)
    - Created reusable components:
       - `StatusFilter.vue` for task filtering with props, emits, and v-model support.
       - `TodoItem.vue` for individual todo rendering with controlled updates and delete events.
     - Updated `App.vue` to import and use new components for better structure and scalability.
     - Applied consistent code style conventions

---
+++ src/components/StatusFilter.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { defineEmits, defineProps } from "vue";
+
+const emit = defineEmits(["update:modelValue"]);
+const { modelValue } = defineProps({
+  modelValue: String,
+});
+</script>
+
+<template>
+  <nav class="filter">
+    <a
+      href="#/"
+      class="filter__link"
+      :class="{ selected: modelValue === 'all' }"
+      @click="emit('update:modelValue', 'all')"
+    >
+      All
+    </a>
+
+    <a
+      href="#/active"
+      class="filter__link"
+      :class="{ selected: modelValue === 'active' }"
+      @click="emit('update:modelValue', 'active')"
+    >
+      Active
+    </a>
+
+    <a
+      href="#/completed"
+      class="filter__link"
+      :calss="{ selected: modelValue === 'completed' }"
+      @click="emit('update:modelValue', 'completed')"
+    >
+      Completed
+    </a>
+  </nav>
+</template>
