6e6f2994ba8fce1649063ed53e668b18491ee4ad
Oleh Senkiv <o.senkiv.v@gmail.com>
2025-10-21 09:18:54 +0300

    Added user interactions with page elements

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,16 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueDevTools from "vite-plugin-vue-devtools";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/new-theory_vue-todo-app_composition-api/',
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  base: "/new-theory_vue-todo-app_composition-api/",
+  plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-})
+});
